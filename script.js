078987658bee08715c2301d58d95f615e7fc8d4a
Anshul Patel <anshul_patel@outlook.com>
2017-09-07 20:17:26 -0400

    Website

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  $('.4u').mouseenter(function () {
+    $(this).fadeTo('fast',0.7);
+  });
+
+  $('.4u').mouseleave(function () {
+    $(this).fadeTo('fast',1);
+  });
+
+});
